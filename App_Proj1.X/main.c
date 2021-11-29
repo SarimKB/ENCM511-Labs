2cbab19a1b30a05bdf5a520e98dabf8f8a2cd26d
SarimKB <sarimk4779@gmail.com>
2021-11-28 20:21:45 -0700

    final

--- App_Proj1.X/main.c
+++ App_Proj1.X/main.c
@@ -56,23 +56,33 @@
 #include "timers.h"
 #include "interrupts.h"
 #include "displayer.h"
+#include <stdbool.h>
 
 #define PB1 PORTAbits.RA2
 #define PB2 PORTAbits.RA4
 #define PB3 PORTBbits.RB4
 #define LED_out LATBbits.LATB8
 
-//Global variable for the time delay
-//uint16_t oneSecDelay = 8000;
-
-// user defined global variable used as a flag
-int CN1flag = 0;
-int CN0flag = 0;
-int CN30flag = 0;
-
 //Timer variables, global
-//unsigned int timeLeft_min = 0;
-//unsigned int timeLeft_sec = 0;
+unsigned int timeLeft_min = 0;
+unsigned int timeLeft_sec = 0;
+
+//Global variable for the time delay
+uint16_t oneSecDelay = 8000;
+
+//Variable keeping track of whether PB3 had a short or long press
+unsigned int long_press = 0;
+
+//Counter keeping track of seconds for PB3
+int counter_pb = 0;
+
+//Variable keeping track of the countdown start or pause
+//0 -> paused
+//1 -> start
+bool CD_start = 0;
+
+//Boolean to toggle LED status
+volatile bool led_toggle = 0;
 
 int main(void) {
     //NewClk(32);
@@ -85,80 +95,186 @@ int main(void) {
     //Initialize pullups for inputs
     PullUpConfig();
     
-    
-  /*
     while(1){
-       
-         
-        //Reset LED
-        //LED_out = 0;
-
-        //When PB1 is pressed. Incrementing minutes count
-        while(PB1 == 0){
-            
-            // check to see if time is at 59 minutes
-            if(timeLeft_min == 59)
-                break;
-            
-            //Incrementing minutes count by 1 minute
-            timeLeft_min += 1;
-
-            //Time for user to see individual time change
-            int x;
-            for(x = 0; x < 1000; x++){               
-                Delay_ms(oneSecDelay);
-            }
-            
-            //Displaying time to terminal
-            dispTimeTerm(timeLeft_min, timeLeft_sec);
-            
-        }
         
-        //When PB2 is pressed. Incrementing seconds count
-        while(PB2 == 0){
+            while(!CD_start){
+                Idle();
+            }
+        
+            // MAIN COUNTDOWN LOOP
+            while(CD_start){
+                
+                led_toggle = !led_toggle;
+                if(timeLeft_sec != 0){
+                    timeLeft_sec -= 1;
+                    dispTimeTerm(timeLeft_min, timeLeft_sec);
+                }
+                else if(timeLeft_min != 0){
+
+                    timeLeft_min -= 1;
+                    timeLeft_sec = 59;
+                    dispTimeTerm(timeLeft_min, timeLeft_sec);
+                }
+                else{
+                    LED_out = 1;
+                    dispTimeTerm(timeLeft_min, timeLeft_sec);
+                    Disp2String(" -- ALARM");
+                    CD_start = 0;
+                    break;
+                }
+                
+                // LED ON
+                if(led_toggle){
+                    LED_out = 1;
+                }
+                // LED OFF
+                else{
+                    LED_out = 0;
+                }
+                Delay_ms(oneSecDelay*2);
+
             
-            // check to see if time is at 59 minutes
+            }// END OF WHILE COUNTDOWN
+        
+    }
+    
+    return 0;
+}
+
+
+// set controller interrupts for button subroutines
+void __attribute__((interrupt, no_auto_psv))_CNInterrupt(void){
+    
+    if(IFS1bits.CNIF == 1){
+        
+        // if PB3 connected to RB4 is pressed
+        // sort out whether long or short press is detected
+        // act accordingly
+        if(PB3 == 0){
+            
+            // start a 16 bit timer and if it hits 3 seconds, stop countdown and reset timer to 0
+            // else, start countdown
+            // set global flag to 1
+            long_press = 0;
+            counter_pb = 0;
+            while(PB3 == 0){
+                counter_pb++;
+                if(counter_pb > 3){
+                    long_press = 1;
+                }
+                
+                int s;
+                for(s = 0; s < 30000; s++){   
+                    
+                    Delay_ms(oneSecDelay);
+                }
+                for(s = 0; s < 30000; s++){   
+                    
+                    Delay_ms(oneSecDelay);
+                }
+                for(s = 0; s < 10000; s++){   
+                    
+                    Delay_ms(oneSecDelay);
+                }   
+            }
+                
+            // see if timer is held down for 3 seconds
+            // in the interrupt, update the display to show 00:00
+            // if the button is released within 3 seconds, start the countdown
+            if(PB3 == 1){
+                
+                // If short press
+                // To start or pause countdown
+                if(long_press == 0){
+                    CD_start = !CD_start;
+                }
+                // If long press
+                // To reset countdown
+                else{
+                    LED_out = 0;
+                    CD_start = 0;
+                    timeLeft_min = 0;
+                    timeLeft_sec = 0;
+                    dispTimeTerm(timeLeft_min, timeLeft_sec);
+                }
+                
+            }// END OF PB3 == 1
+               
+        }// END OF PB3
+        
+        
+        // if PB2 connected to RA4 is pressed
+        // set the seconds to be counted down. 
+        // in increments of 1 second, increase seconds by 1 from 0 to 59
+        // clear alarm message from terminal
+        if(PB2 == 0){
+            
+            // while button is pressed
+            while(PB2 == 0){
+                int z;
+                for(z = 0; z < 30000; z++){   
+                    
+                    Delay_ms(oneSecDelay);
+                }
+                for(z = 0; z < 10000; z++){   
+                    
+                    Delay_ms(oneSecDelay);
+                }
+                // check to see if time is at 59 minutes
                 if(timeLeft_sec == 59)
                     break;
             
                 //Incrementing minutes count by 1 minute
-                timeLeft_min += 1;
-
-                
-//                LED_out = 1;
-//                Delay_ms(oneSecDelay);
-//                LED_out = 0;
-//                Delay_ms(oneSecDelay);
-                //Time for user to see individual time change
-                int x;
-                for(x = 0; x < 1000; x++){               
-                    Delay_ms(oneSecDelay);
-                }
+                timeLeft_sec += 1;
                 
                 //Displaying time to terminal
                 dispTimeTerm(timeLeft_min, timeLeft_sec);
             
-        }
-             
-    }
+            }// END OF WHILE
+            
+        }//END OF PB2
+        
+        
+        // if PB1 connected to RA2 is pressed
+        // set the minutes to be counted down. 
+        // in increments of 1 minute, increase minutes by 1 from 0 to 59
+        // clear alarm message from terminal
+        if(PB1 == 0){
+            
+            // while button is pressed
+            while(PB1 == 0){
+                
+               int w;
+               for(w = 0; w < 30000; w++){   
+                    
+                Delay_ms(oneSecDelay);
+               }
+               for(w = 0; w < 10000; w++){   
+                    
+                Delay_ms(oneSecDelay);
+               }
+                
+               // check to see if time is at 59 minutes
+               if(timeLeft_min == 59)
+                   break;
+            
+               //Incrementing minutes count by 1 minute
+               timeLeft_min += 1;
+                
+               //Displaying time to terminal
+               dispTimeTerm(timeLeft_min, timeLeft_sec);
+            
+            }
+            
+        }//END OF PB1
+        
+    }//END OF MAIN IF
     
-//    while(1){
-//        // loop to run idle / sleep mode
-//        Idle();
-////        uint16_t delay = 16000;
-////        LED_out = 1;
-////        Delay_ms(delay);
-////        LED_out = 0;
-////        Delay_ms(delay);
-//    }
-         */
-    while(1){
-        Idle();
-    }
+    // clearing the IF flag
+    IFS1bits.CNIF = 0;
     
-   
-    return 0;
 }
+
 
 
 
